e4ed2db018131f91aceddcdc5700e7969edef74f
MiguelM <migueama29@gmail.com>
2024-07-21 22:30:43 -0600

    Initial commit

---
+++ app/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, Float, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+from sqlalchemy.orm import declarative_base
+
+
+
+Base = declarative_base()
+
+class Producto(Base):
+    __tablename__ = 'productos'
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String, index=True)
+    precio = Column(Float)
+    cantidad = Column(Integer)
+    fecha_creacion = Column(DateTime, default=datetime.utcnow)
+    categoria = Column(String)
